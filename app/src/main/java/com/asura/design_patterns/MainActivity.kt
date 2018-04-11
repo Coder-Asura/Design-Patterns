a0c2f2827ce902f255d50ed55642db2eae107d9f
Asura <709238339@qq.com>
2018-04-11 15:14:56 +0800

    增加原型模式

--- app/src/main/java/com/asura/design_patterns/MainActivity.kt
+++ app/src/main/java/com/asura/design_patterns/MainActivity.kt
@@ -10,6 +10,8 @@ import com.asura.design_patterns.bulider.step0.Director
 import com.asura.design_patterns.bulider.step0.MacBookBuilder
 import com.asura.design_patterns.bulider.step1.SurfaceBuilder
 import com.asura.design_patterns.principle.SixPrinciplesActivity
+import com.asura.design_patterns.prototype.step0.WordDoc
+import com.asura.design_patterns.singleinstance.step0.CEO
 import com.asura.design_patterns.singleinstance.step5.SingletonManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -60,56 +62,121 @@ class MainActivity : AppCompatActivity() {
     private fun viewDetails(position: Int) {
         when (position) {
             0 -> {
-                val intent = Intent()
-                intent.putExtra("content", resources.getString(R.string.app_name))
-                intent.setClass(this@MainActivity, SixPrinciplesActivity::class.java)
-                startActivity(intent)
+                principleDemo()
             }
             1 -> {
-                //创建单例
-                val ceo0 = com.asura.design_patterns.singleinstance.step0.CEO.getCEO()
-                val ceo1 = com.asura.design_patterns.singleinstance.step1.CEO.getCEO()
-                val ceo2 = com.asura.design_patterns.singleinstance.step2.CEO.getInstance()
-                val ceo3 = com.asura.design_patterns.singleinstance.step3.CEO.getInstance()
-                val ceo4 = com.asura.design_patterns.singleinstance.step4.CEO.INSTANCE
-                //加入到集合容器中管理
-                SingletonManager.registerService("ceo0", ceo0)
-                SingletonManager.registerService("ceo1", ceo1)
-                SingletonManager.registerService("ceo2", ceo2)
-                SingletonManager.registerService("ceo3", ceo3)
-                SingletonManager.registerService("ceo4", ceo4)
-                //重新获取一次单例
-                val ceo02 = com.asura.design_patterns.singleinstance.step0.CEO.getCEO()
-                val ceo12 = com.asura.design_patterns.singleinstance.step1.CEO.getCEO()
-                val ceo22 = com.asura.design_patterns.singleinstance.step2.CEO.getInstance()
-                val ceo32 = com.asura.design_patterns.singleinstance.step3.CEO.getInstance()
-                val ceo42 = com.asura.design_patterns.singleinstance.step4.CEO.INSTANCE
-                //比较两次实例是否相同
-                Log.d("asura", (ceo02 == ceo0).toString());
-                Log.d("asura", (ceo12 == ceo1).toString());
-                Log.d("asura", (ceo22 == ceo2).toString());
-                Log.d("asura", (ceo32 == ceo3).toString());
-                Log.d("asura", (ceo42 == ceo4).toString());
-                //和容器里的单例做比较
-                Log.d("asura", (ceo02 == SingletonManager.getService("ceo0")).toString());
-                Log.d("asura", (ceo12 == SingletonManager.getService("ceo1")).toString());
-                Log.d("asura", (ceo22 == SingletonManager.getService("ceo2")).toString());
-                Log.d("asura", (ceo32 == SingletonManager.getService("ceo3")).toString());
-                Log.d("asura", (ceo42 == SingletonManager.getService("ceo4")).toString());
+                singleInstanceDemo()
             }
             2 -> {
-                val builder = MacBookBuilder()
-                val director = Director(builder)
-                director.construct("Apple", "Apple Retina Display")
-                val computer = builder.build()
-                Log.d("asura", computer.toString())
-
-                val builder1 = SurfaceBuilder()
-                val computer1 = builder1.buildBoard("ASUS B350")
-                        .buildDisplay("SamSung 34\" Display")
-                        .build()
-                Log.d("asura", computer1.toString())
+                builderDemo()
+            }
+            3 -> {
+                prototypeDemo()
             }
         }
+    }
+
+    private fun prototypeDemo() {
+        // 浅拷贝 demo
+        Log.d("asura", "----------浅拷贝 demo------------")
+        Log.d("asura", "----------创建原文档------------")
+        val originDoc = WordDoc()
+        originDoc.text = "这是第一个文档"
+        originDoc.imags.add("苍井空.jpg")
+        originDoc.imags.add("饭岛爱.jpg")
+        originDoc.imags.add("天使翼.jpg")
+        originDoc.showDoc()
+        Log.d("asura", "----------clone文档------------")
+        val cloneDoc = originDoc.clone()
+        cloneDoc.showDoc()
+        Log.d("asura", "----------修改clone的文档 text------------")
+        cloneDoc.text = "修改原文档里的内容"
+        cloneDoc.showDoc()
+        Log.d("asura", "----------修改clone后的原文档------------")
+        originDoc.showDoc()
+        Log.d("asura", "----------修改clone的文档 imags------------")
+        //浅拷贝会影响原型对象
+        cloneDoc.imags.add("武藤兰.jpg")
+        cloneDoc.showDoc()
+        Log.d("asura", "----------修改clone后的原文档------------")
+        originDoc.showDoc()
+
+        //深拷贝 demo
+        Log.d("asura", "----------深拷贝 demo------------")
+        Log.d("asura", "----------创建原文档------------")
+        val originDoc1 = com.asura.design_patterns.prototype.step1.WordDoc()
+        originDoc1.text = "这是第一个文档"
+        originDoc1.imags.add("苍井空.jpg")
+        originDoc1.imags.add("饭岛爱.jpg")
+        originDoc1.imags.add("天使翼.jpg")
+        originDoc1.showDoc()
+        Log.d("asura", "----------clone文档------------")
+        val cloneDoc1 = originDoc1.clone()
+        cloneDoc1.showDoc()
+        Log.d("asura", "----------修改clone的文档 text------------")
+        cloneDoc1.text = "修改原文档里的内容"
+        cloneDoc1.showDoc()
+        Log.d("asura", "----------修改clone后的原文档------------")
+        originDoc1.showDoc()
+        Log.d("asura", "----------修改clone的文档 imags------------")
+        //浅拷贝会影响原型对象
+        cloneDoc1.imags.add("武藤兰.jpg")
+        cloneDoc1.showDoc()
+        Log.d("asura", "----------修改clone后的原文档------------")
+        originDoc1.showDoc()
+    }
+
+    private fun builderDemo() {
+        val builder = MacBookBuilder()
+        val director = Director(builder)
+        director.construct("Apple", "Apple Retina Display")
+        val computer = builder.build()
+        Log.d("asura", computer.toString())
+
+        val builder1 = SurfaceBuilder()
+        val computer1 = builder1.buildBoard("ASUS B350")
+                .buildDisplay("SamSung 34\" Display")
+                .build()
+        Log.d("asura", computer1.toString())
+    }
+
+    private fun singleInstanceDemo() {
+        //创建单例
+        val ceo0 = CEO.getCEO()
+        val ceo1 = com.asura.design_patterns.singleinstance.step1.CEO.getCEO()
+        val ceo2 = com.asura.design_patterns.singleinstance.step2.CEO.getInstance()
+        val ceo3 = com.asura.design_patterns.singleinstance.step3.CEO.getInstance()
+        val ceo4 = com.asura.design_patterns.singleinstance.step4.CEO.INSTANCE
+        //加入到集合容器中管理
+        SingletonManager.registerService("ceo0", ceo0)
+        SingletonManager.registerService("ceo1", ceo1)
+        SingletonManager.registerService("ceo2", ceo2)
+        SingletonManager.registerService("ceo3", ceo3)
+        SingletonManager.registerService("ceo4", ceo4)
+        //重新获取一次单例
+        val ceo02 = CEO.getCEO()
+        val ceo12 = com.asura.design_patterns.singleinstance.step1.CEO.getCEO()
+        val ceo22 = com.asura.design_patterns.singleinstance.step2.CEO.getInstance()
+        val ceo32 = com.asura.design_patterns.singleinstance.step3.CEO.getInstance()
+        val ceo42 = com.asura.design_patterns.singleinstance.step4.CEO.INSTANCE
+        //比较两次实例是否相同
+        Log.d("asura", (ceo02 == ceo0).toString());
+        Log.d("asura", (ceo12 == ceo1).toString());
+        Log.d("asura", (ceo22 == ceo2).toString());
+        Log.d("asura", (ceo32 == ceo3).toString());
+        Log.d("asura", (ceo42 == ceo4).toString());
+        //和容器里的单例做比较
+        Log.d("asura", (ceo02 == SingletonManager.getService("ceo0")).toString());
+        Log.d("asura", (ceo12 == SingletonManager.getService("ceo1")).toString());
+        Log.d("asura", (ceo22 == SingletonManager.getService("ceo2")).toString());
+        Log.d("asura", (ceo32 == SingletonManager.getService("ceo3")).toString());
+        Log.d("asura", (ceo42 == SingletonManager.getService("ceo4")).toString());
+    }
+
+    private fun principleDemo() {
+        val intent = Intent()
+        intent.putExtra("content", resources.getString(R.string.app_name))
+        intent.setClass(this@MainActivity, SixPrinciplesActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/asura/design_patterns/prototype/step1/WordDoc.kt
@@ -0,0 +1,33 @@
+package com.asura.design_patterns.prototype.step1
+
+import android.util.Log
+
+/**
+ * @author Created by Asura on 2018/4/11 14:30.
+ */
+class WordDoc : Cloneable {
+    var text: String? = ""
+    var imags = ArrayList<String>()
+
+    fun showDoc() {
+        Log.d("asura", "Word Content Start")
+        Log.d("asura", "text:$text")
+        imags.forEach {
+            Log.d("asura", "imags:" + it)
+        }
+        Log.d("asura", "Word Content End")
+    }
+
+    public override fun clone(): WordDoc {
+        val doc = super.clone() as WordDoc
+        doc.text = this.text
+        //这里 imags 改成深拷贝
+        doc.imags = this.imags.clone() as ArrayList<String>
+        return doc
+    }
+
+    //clone 产生的实例不会调用构造方法
+    init {
+        Log.d("asura", "WordDoc 的构造函数")
+    }
+}
